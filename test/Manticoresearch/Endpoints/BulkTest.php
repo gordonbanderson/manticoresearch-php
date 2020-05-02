8cb0c13ac004f64f72bb858ad1915dbd3de232c4
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-05-02 13:48:10 +0100

    WIP: Added transport to params

--- test/Manticoresearch/Endpoints/BulkTest.php
+++ test/Manticoresearch/Endpoints/BulkTest.php
@@ -9,7 +9,13 @@ class BulkTest extends \PHPUnit\Framework\TestCase
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
-        $params = ['host' => $_SERVER['MS_HOST'], 'port' => $_SERVER['MS_PORT']];
+        $params = [
+            'host' => $_SERVER['MS_HOST'],
+            'port' => $_SERVER['MS_PORT'],
+            'transport' => empty($_SERVER['TRANSPORT']) ? 'Http' : $_SERVER['TRANSPORT']
+        ];
+        error_log(print_r($params, 1));
+
         static::$client = new Client($params);
         $params = [
             'index' => 'bulktest',
